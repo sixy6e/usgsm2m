04b7e6102ef035ff087c617b963c6602975077a8
sixy6e <josh.sixsmith@gmail+GitHub.com>
2026-05-21 20:59:42 +1000

    Update/fix: fix filter types AND and VALUE to lowercase. Added new types to cater for the dataset filters; DatasetFiltersResponse DatasetFilterField FieldConfigDetail

--- pkg/usgsm2m/filters.go
+++ pkg/usgsm2m/filters.go
@@ -87,7 +87,7 @@ func NewMetadataFilter(opts ...MetadataOption) MetadataFilter {
 // WithValue sets the VALUE type and its fields
 func WithValue(id string, val interface{}, operand string) MetadataOption {
 	return func(f *MetadataFilter) {
-		f.FilterType = "VALUE"
+		f.FilterType = "value"
 		f.FilterId = id
 		f.Value = val
 		f.Operand = operand
@@ -97,7 +97,7 @@ func WithValue(id string, val interface{}, operand string) MetadataOption {
 // WithAnd nests other filters in an AND block
 func WithAnd(filters ...MetadataFilter) MetadataOption {
 	return func(f *MetadataFilter) {
-		f.FilterType = "AND"
+		f.FilterType = "and"
 		f.Filters = filters
 	}
 }
@@ -151,4 +151,31 @@ type TemporalFilter struct {
 	Start time.Time `json:"start"`
 	// ISO 8601 Formatted Date
 	End time.Time `json:"end"`
+}
+
+// DatasetFiltersResponse mirrors the top-level envelope returned by the 'dataset-filters' endpoint
+type DatasetFiltersResponse struct {
+	RequestID    int64                `json:"requestId"`
+	Version      string               `json:"version"`
+	Data         []DatasetFilterField `json:"data"`
+	ErrorCode    string               `json:"errorCode,omitempty"`
+	ErrorMessage string               `json:"errorMessage,omitempty"`
+}
+
+// DatasetFilterField represents an individual searchable filter definition asset template
+type DatasetFilterField struct {
+	ID             string            `json:"id"`
+	LegacyFieldID  interface{}       `json:"legacyFieldId"` // Can be a null JSON block or integer
+	DictionaryLink string            `json:"dictionaryLink"`
+	FieldLabel     string            `json:"fieldLabel"`
+	SearchSQL      string            `json:"searchSql"`
+	FieldConfig    FieldConfigDetail `json:"fieldConfig"`
+	ValueList      map[string]string `json:"valueList,omitempty"` // Option maps e.g., {"8": "8", "9": "9"}
+}
+
+// FieldConfigDetail holds validation rules and interactive UI properties from USGS
+type FieldConfigDetail struct {
+	Type          string      `json:"type"` // "Text", "Range", "Select"
+	NumElements   interface{} `json:"numElements,omitempty"`
+	DisplayListID string      `json:"displayListId,omitempty"`
 }
