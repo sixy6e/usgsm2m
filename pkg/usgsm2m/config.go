ecccf251c493832d6b97744d1ffb191795ab9c5c
sixy6e <josh.sixsmith@gmail+GitHub.com>
2026-05-23 18:57:00 +1000

    Update: reworked config to have clear nested structure rather than a flat/squashed structure.

--- pkg/usgsm2m/config.go
+++ pkg/usgsm2m/config.go
@@ -1,8 +1,16 @@
 package usgsm2m
 
 type Config struct {
-	Username    string `mapstructure:"username"`
-	Token       string `mapstructure:"token"`
+	Auth     AuthConfig    `mapstructure:"auth"`
+	Defaults DefaultParams `mapstructure:"defaults"`
+}
+
+type AuthConfig struct {
+	Username string `mapstructure:"username"`
+	Token    string `mapstructure:"token"`
+}
+
+type DefaultParams struct {
 	OutputDir   string `mapstructure:"output_dir"`
 	Concurrency int    `mapstructure:"concurrency"`
 	Dataset     string `mapstructure:"dataset"`
